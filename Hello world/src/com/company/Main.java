75bbdbe552a1d3dfa979ee897d3e0a01c20c535a
Md.Tanvir Ahmed <67621193+MdTanvirAhemd@users.noreply.github.com>
2021-09-06 20:11:22 +0600

    Add files via upload

---
+++ Hello world/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String ourFriendName = "John";
+        int age = 24;
+        System.out.println("Our friend "+ourFriendName+" is " +age+ " year old.");
+    }
+}
---
+++ test/src/PersonTest.java
@@ -0,0 +1,7 @@
+
+public class PersonTest {
+    public static void main(String[] args) {
+        String str1= "8a3";
+        System.out.println(str1.matches("[0-9][a-z][0-9]"));
+    }
+}
